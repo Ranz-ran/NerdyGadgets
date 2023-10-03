84a7c8885d2c0bb4c930a9277d1461721401fe39
Troy <troy.stilting@st-2.com>
2023-10-03 14:39:52 +0200

    new home page (+css file) pushed

---
+++ Homepage/index.php
@@ -0,0 +1,60 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- responsive code -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- external links -->
+    <link rel="stylesheet" href="css.css">
+    <title>Nerdy Gadgets</title>
+</head>
+<header>
+    <!-- het logo -->
+    <div class="logo">
+    <img src="" alt="Nerdy Gadgets Logo">
+    </div>
+
+    <!-- de search bar -->
+    <div class="search">
+
+    </div>
+
+    <!-- displayt de shopping cart -->
+    <div class="de legend">
+        <p>	&#128722 </p>
+    </div>
+
+    <!-- profile en login -->
+    <div class="profile">
+        <img src="" alt="Profile Picture">
+    </div>
+</header>
+<nav>
+    <!-- vul hier uw pagina's in voor toekomstige sprints -->
+    <a href="#">sample</a>
+</nav>
+<body>
+    <!-- als je wil kan je hier bijvoorbeeld de slogan plakken als je wilt -->
+    <div class="slogan">
+
+    </div>
+    <!-- aanbevolen producten slider -->
+    <div class="aanbevolen">
+        <div class="slides">
+            <div class="slide"><img src="image1.jpg" alt="Slide 1"></div>
+            <div class="slide"><img src="image2.jpg" alt="Slide 2"></div>
+            <div class="slide"><img src="image3.jpg" alt="Slide 3"></div>
+
+            <button class="prev" onclick="plusSlides(-1)">&#10094;</button>
+            <button class="next" onclick="plusSlides(1)">&#10095;</button>
+        </div>
+    </div>
+</body>
+<footer>
+    <!-- indien nodig? -->
+</footer>
+</html>
