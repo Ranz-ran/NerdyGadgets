f1b6995ee591703cdd8ba4e27c936b4923fa101f
Rita <ritakuo2002@gmail.com>
2023-10-03 14:42:54 +0200

    files update + rename

--- Homepage/homepage.php
+++ Homepage/homepage.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- external links -->
-    <link rel="stylesheet" href="css.css">
+    <link rel="stylesheet" href="homepage.css">
     <title>Nerdy Gadgets</title>
 </head>
 <header>
