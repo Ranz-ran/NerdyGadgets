8524d19bb685f75c5e30253d14b4ad5e927e904b
Rita <ritakuo2002@gmail.com>
2023-10-06 09:39:54 +0200

    test

--- HetBestek/index.php
+++ HetBestek/index.php
@@ -54,6 +54,7 @@ include "./header.php";
 
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<test></test>
 </body>
 <?php include "./footer.php" ?>
 </html>
