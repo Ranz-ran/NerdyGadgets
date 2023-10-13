585bcef90abcece4c4ea3e76ef9d789ee2f3e6a2
Amber <amber.adrichem@gmail.com>
2023-10-13 11:05:47 +0200

    poging tot header

--- HetBestek/index.php
+++ HetBestek/index.php
@@ -17,12 +17,12 @@ include "../Algemeen/navigation menu.html";
 
 
 </head>
-
+<div class="header">
 <div class="logo">
     <a href="#">
         <img src="../Img/IMG_1915.png" alt="Nerdy Gadgets Logo" class="logo">
     </a>
-</div>
+</div> </div>
 <body>
     <!-- als je wil kan je hier bijvoorbeeld de slogan plakken als je wilt -->
     <div class="slogan">
